877a029c397ec0bf504105c913fe083aa686e266
Tim Vasko <tvasko@galacticengineering.com>
2017-08-24 19:50:02 -0700

    added sample project for quick deployments

---
+++ shell/dev/src/main.cpp
@@ -0,0 +1,14 @@
+//
+// main.cpp
+//
+// Copyright © 2017. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Hello world!" << std::endl;
+	return 0;
+}
+
