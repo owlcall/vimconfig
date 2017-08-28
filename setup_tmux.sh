1570bdd8bc2d4f0799128cc218376f3bf4ac44ca
Tim Vasko <tvasko@galacticengineering.com>
2017-08-27 17:50:39 -0700

    setup_tmux.sh

--- setup_tmux.sh
+++ setup_tmux.sh
@@ -13,7 +13,7 @@ cd temp_tmux
 curl -L https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz > libevent.tar.gz
 tar vxf libevent*
 cd libevent*
-./configure --prefix=/usr/local
+./configure --prefix=`echo ~`/opt/local
 make -j 4
 make install
 cd ..
@@ -23,7 +23,7 @@ cd ..
 curl -L https://ftp.gnu.org/gnu/ncurses/ncurses-6.0.tar.gz > ncurses.tar.gz
 tar vxf ncurses*
 cd ncurses*
-./configure --prefix=/usr/local
+./configure --prefix=`echo ~`/opt/local
 make -j 4
 make install
 cd ..
@@ -33,7 +33,7 @@ cd ..
 curl -L https://github.com/tmux/tmux/releases/download/2.5/tmux-2.5.tar.gz > tmux.tar.gz
 tar vxf tmux*
 cd tmux*
-./configure --prefix=/usr/local
+./configure --prefix=`echo ~`/opt/local
 make -j 4
 make install
 cd ..
