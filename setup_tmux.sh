c8ed89b92b05ac1bef8f066b4a3a1afcb54c89c6
Tim Vasko <tvasko@galacticengineering.com>
2017-08-29 21:32:06 -0700

    Fixed tmux installer

--- setup_tmux.sh
+++ setup_tmux.sh
@@ -7,12 +7,10 @@
 # ----- tmux -----
 # Latest release: https://github.com/tmux/tmux/releases
 curl -L https://github.com/tmux/tmux/releases/download/2.5/tmux-2.5.tar.gz > tmux.tar.gz
-tar vxf tmux*
+tar vxf tmux.tar.gz
 rm tmux.tar.gz
 cd tmux-2.5
-export DIR="/usr/local"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DIR/lib
-./configure --prefix=$DIR CFLAGS="-I$DIR/include" LDFLAGS="-L$DIR/lib"
+./configure LDFLAGS='-Wl,-rpath,/usr/local/lib'
 make -j 4
 cd ..
 
