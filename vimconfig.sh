1a9fde7838b6623ad6d22f7caf0251340de797f1
Tim Vasko <tvasko@tree>
2018-02-17 22:45:56 -0800

    fixed issues in configuration script

--- vimconfig.sh
+++ vimconfig.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 
-DIR=$( cd "$( dirname "$0" )" && pwd )
+DIR=$( cd "$( dirname "$0" )" &&  pwd )
 OP="ln -sf"
 FORCE=false
 
@@ -36,7 +36,7 @@ done
 # - install vim plugins
 
 # Backup old ~/.vimrc if it already exists
-if [ -f ~/.vimrc && ! -L ~/.vimrc ]; then
+if [ -f ~/.vimrc ]  &&  [ ! -L ~/.vimrc ]; then
 	if [ ! $FORCE ]; then
 		echo "~/.vimrc_backup already exists; use -f or --force to overwrite" 1>&2
 		exit 1
@@ -52,20 +52,20 @@ mkdir -p ~/.vim/colors
 $OP "$DIR/configs/vim/apprentice.vim" ~/.vim/colors/apprentice.vim
 
 # Install plugins
-if [ ! -d ~/.vim/bundle ]; then mkdir -p ~/.vim/bundle;	fi
+mkdir -p ~/.vim/bundle
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 	vim +PluginInstall +qall
 fi
 
 # Configure vim airline theme
-$OP "$ROOT/configs/vim/airline_bubblegum.vim" ~/.vim/bundle/vim-airline-themes/autoload/airline/themes/bubblegum.vim
+$OP "$DIR/configs/vim/airline_bubblegum.vim" ~/.vim/bundle/vim-airline-themes/autoload/airline/themes/bubblegum.vim
 
 # Configure vim syntax coloring
 for d in [ "syntax", "after/syntax" ]; do
 	mkdir -p ~/.vim/$d
 	for i in [ "$DIR/configs/vim/$d/*" ]; do
-		if [ -f ~/.vim/$d/$i && ! $FORCE ]; then
+		if [ -f ~/.vim/$d/$i ]  &&  [ ! $FORCE ]; then
 			echo "~/.vim/$d/$i already exists; use -f or --force to overwrite" 1>&2
 			exit 1
 		fi
